994e41638f495b6ddccd19148d48680798cea37b
jason <1207436809@qq.com>
2025-05-14 23:24:08 +0800

    build: 添加自定义打包配置以优化构建输出
    引入 `setupBuild` 函数来配置 Vite 的构建选项，包括输出目录、静态资源分类打包以及消除打包大小警告。此更改旨在提高构建过程的灵活性和性能。

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { setupBuild } from './build/index'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: setupBuild(),
   plugins: [vue()],
-  base: './', // 设置基础路径，确保在GitHub Pages上正确加载资源
-})
+  base: "./", // 设置基础路径，确保在GitHub Pages上正确加载资源
+});
